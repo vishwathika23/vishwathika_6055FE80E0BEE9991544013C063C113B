e9081ac6a3fa6b1995f0319eb45e2b413a485783
vishwathika23 <144264926+vishwathika23@users.noreply.github.com>
2023-09-25 21:05:41 +0530

    Add files via upload

---
+++ CHALLENE3/main (4).py
@@ -0,0 +1,11 @@
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+  return indices
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target1 = 'fruit'
+result = linearSearchProduct(products, target)
+print(result)
